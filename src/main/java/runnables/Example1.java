5ec8da2b698ac2c88f65137ddc19d08b9c9082be
Simon Roberts <simon@dancingcloudservices.com>
2020-05-07 14:12:43 -0600

    Final

---
+++ src/main/java/runnables/Example1.java
@@ -0,0 +1,29 @@
+package runnables;
+
+class MyJob implements Runnable {
+  int i = 0;
+  @Override
+  public void run() {
+    System.out.println(Thread.currentThread().getName()
+        + " MyRunnable starting...");
+    for (; i < 10_000; i++) {
+      System.out.println(Thread.currentThread().getName()
+          + " i is " + i);
+    }
+    System.out.println(Thread.currentThread().getName()
+        + " MyRunnable ending...");
+  }
+}
+
+public class Example1 {
+  public static void main(String[] args) {
+    Runnable r = new MyJob();
+    Thread t = new Thread(r);
+//    t.run(); // WRONG!!!!
+    t.start(); // RIGHT!!!!
+    Thread t2 = new Thread(r);
+    t2.start();
+    System.out.println(Thread.currentThread().getName()
+        + " main method ending...");
+  }
+}
---
+++ src/main/java/runnables/Stopper.java
@@ -0,0 +1,21 @@
+package runnables;
+
+public class Stopper {
+  public static volatile boolean stop = false;
+
+  public static void main(String[] args) throws Throwable {
+    Runnable r = () ->{
+      System.out.println("Stopper job started...");
+      while (! stop)  // Timing???
+        ;
+      System.out.println("Stopper job completed...");
+    };
+
+    new Thread(r).start();
+    System.out.println("started...");
+    Thread.sleep(1_000);
+    System.out.println("about to set stop flag...");
+    stop = true;
+    System.out.println("Stop set to "  + stop);
+  }
+}
